14ad474684ac9e107af5e014166d9f78546220df
BSupriano <128910792+BrunoSupriano@users.noreply.github.com>
2024-10-17 20:37:08 -0300

    first commit

---
+++ src/main/java/com/example/dgc/Clientes/ClientController.java
@@ -0,0 +1,35 @@
+package com.example.dgc.Clientes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cliente")
+public class ClientController {
+    @Autowired
+    private ClientService ClientService;
+
+    @GetMapping
+    public List<ClientModel> listarTodos() {
+        return ClientService.listarTodos();
+    }
+
+    @PostMapping
+    public ClientModel salvar(@RequestBody ClientModel cliente) {
+        return ClientService.salvar(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public ClientModel atualizar(@PathVariable Long id, @RequestBody ClientModel cliente) {
+        return ClientService.atualizar(id, cliente);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        ClientService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
