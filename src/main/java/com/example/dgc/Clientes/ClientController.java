b7068fe7dd9211ddb2e37c68110e044600c9f67b
BSupriano <128910792+BrunoSupriano@users.noreply.github.com>
2024-10-19 10:55:32 -0300

    Implementado crud cliente

--- src/main/java/com/example/dgc/Clientes/ClientController.java
+++ src/main/java/com/example/dgc/Clientes/ClientController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000") // Ou o domínio do seu app Next.js
 @RestController
 @RequestMapping("/cliente")
 public class ClientController {
